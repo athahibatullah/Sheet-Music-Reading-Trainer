32a1a3744dd81908aeb04d803b59e4aa1e29af81
Atha Hibatullah <athalayana@mail.ugm.ac.id>
2020-09-24 00:33:43 +0700

    Added piano display

--- Sheet Music.py
+++ Sheet Music.py
@@ -3,6 +3,7 @@ sys.path.insert(1,'Lib\site-packages') #to get all the package
 import pygame
 import pygame.midi
 from pygame.locals import *
+# import random
 
 pygame.init()
 
@@ -16,8 +17,18 @@ event_get = pygame.fastevent.get
 event_post = pygame.fastevent.post
 
 pygame.display.set_caption("Sheet Music Read")
-screen = pygame.display.set_mode((400, 300), RESIZABLE, 32)
+screen = pygame.display.set_mode((1200, 700), RESIZABLE, 32)
+font = pygame.font.Font('freesansbold.ttf', 32) 
 
+black = (0,0,0)
+white = (255, 255, 255) 
+green = (0, 255, 0) 
+blue = (0, 0, 128) 
+
+# text = font.render('Sheet Music Reading Trainer', True, green, blue) 
+# textRect = text.get_rect()  
+
+# textRect.center = (800 // 2, 300 // 2) 
 #print(i)
 
 NotelistSharp = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
@@ -40,7 +51,40 @@ def NoteDecider(Note):
 
 
 going = True
+posisix = 25
+posisiy = 550
+panjangtuts = 60
+lebartuts = 11
 while going:
+    screen.fill(white)
+    #pygame.draw.rect(screen,blue,(25, 550, 1145, 100), 3)
+    # while(bikin):
+    for j in range(52):
+        pygame.draw.rect(screen,black,(posisix,posisiy,22,100),2)
+        if(j % 7 == 1):
+            pygame.draw.rect(screen,black,(posisix-4,posisiy,lebartuts,panjangtuts))
+        if(j % 7 == 3):
+            pygame.draw.rect(screen,black,(posisix-6,posisiy,lebartuts,panjangtuts))
+        if(j % 7 == 4):
+            pygame.draw.rect(screen,black,(posisix-4,posisiy,lebartuts,panjangtuts))
+        if(j % 7 == 6):
+            pygame.draw.rect(screen,black,(posisix-6,posisiy,lebartuts,panjangtuts))
+        if(j % 7 == 0 and j != 0):
+            pygame.draw.rect(screen,black,(posisix-5,posisiy,lebartuts,panjangtuts))
+        posisix += 22
+
+    posisix = 25
+
+    # if(random.randint(0,9) == 3):
+    #         pygame.draw.rect(screen,blue,(posisix+(22*random.randint(0,52)),posisiy,22,100))
+
+    # pygame.draw.rect(screen,black,(50,550,30,100),2)
+    #     bikins += 1 
+    #     posisix += 20
+    #     if(bikins == 52):
+    #         bikin = False
+    # bikin = True
+    # screen.blit(text, textRect)
     events = event_get()
     for e in events:
         if e.type in [QUIT]:
@@ -55,6 +99,7 @@ while going:
             # print ("full midi_events " + str(NoteRead))
             # print ("my midi note is " + str(NoteRead[0][0][1]))
             print(NoteDecider(NoteRead[0][0][1]))
+    pygame.display.update()  
 
 print("exit button clicked.")
 i.close()
